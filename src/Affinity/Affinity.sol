5c49a5dc3761029c561ca7c2842ecb2349b1d230
Richard Ryan <richardryangarcia@gmail.com>
2024-06-18 17:39:42 -0500

    rm upgradeable

--- src/Affinity/Affinity.sol
+++ src/Affinity/Affinity.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 import "../interfaces/IAffinity.sol";
-import {ERC1155} from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
+
 
 
 contract Affinity is IAffinity {
